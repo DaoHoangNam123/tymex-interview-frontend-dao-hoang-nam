47c5830f35d48e6036e5d9dc91f9adf23ec1f2a8
EAD\ndao8 <namhoang.dao@dxc.com>
2025-03-28 23:17:07 +0700

    change color skeleton and color reset button

--- src/components/Skeleton/skeleton.tsx
+++ src/components/Skeleton/skeleton.tsx
@@ -5,34 +5,20 @@ import { List, Skeleton } from "antd";
 const SkeletonCard = () => {
   return (
     <div className="relative p-4 rounded-[10px] h-[365px] w-[267px] shadow-xl bg-[#2a2936]">
-      <Skeleton.Node
-        active
-        style={{ width: 235, height: 197, background: "gray" }}
-      />
+      <Skeleton.Node active style={{ width: 235, height: 220 }} />
       <div className="content flex flex-col items-center justify-center pt-6">
         <div className="flex items-center justify-between w-full">
-          <Skeleton.Node
-            active
-            style={{ width: 100, height: 24, background: "gray" }}
-          />
-          <Skeleton.Node
-            active
-            style={{ width: 100, height: 24, background: "gray" }}
-          />
+          <Skeleton.Node active style={{ width: 100, height: 30 }} />
+          <Skeleton.Node active style={{ width: 100, height: 30 }} />
         </div>
         <div className="flex items-center justify-start mt-4 text-sm w-full">
           <Skeleton.Avatar
             active
-            size={"small"}
+            size={"default"}
             shape={"circle"}
             className="mr-3"
-            style={{ background: "gray" }}
           />
-          <Skeleton.Input
-            active
-            size={"small"}
-            style={{ background: "gray" }}
-          />
+          <Skeleton.Input active size={"default"} />
         </div>
       </div>
     </div>
